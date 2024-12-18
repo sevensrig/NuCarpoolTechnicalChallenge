92b9990c3fe86a1fec67977d93a0e510ca914ece
sevensrig <srinivasan.sri@northeastern.edu>
2024-12-18 15:17:58 -0500

    Bare bones of potential match tiles

--- src/MatchingTiles.tsx
+++ src/MatchingTiles.tsx
@@ -1,8 +1,22 @@
-
+import { matchList } from './data/sample_carpool_data'
 
 function MatchingTiles() {
     return(
-        <div></div>
+        <>
+            <div>
+                <p>Name</p>
+                <p>employer</p>
+                <p>start location</p>
+                <p>End location</p>
+                <p>Distance from user start</p>
+                <p>Distance from user end</p>
+                <div>
+                    <button>up</button>
+                    <button>down</button>
+                </div>
+            </div>
+        </>
+        
     );
 }
 
