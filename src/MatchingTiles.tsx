fcf529c87ed919c7191091061dd79a843e7cad4b
sevensrig <srinivasan.sri@northeastern.edu>
2024-12-18 15:46:48 -0500

    Like and dislike buttons advance matches

--- src/MatchingTiles.tsx
+++ src/MatchingTiles.tsx
@@ -1,18 +1,30 @@
 import { matchList } from './data/sample_carpool_data'
+import { useState } from 'react';
 
 function MatchingTiles() {
+    const [ index, setIndex ] = useState(0)
+    const [ currPotentialMatch, setCurrPotentialMatch ] = useState(matchList[0])
+    function handleLikeButton() {
+        setCurrPotentialMatch(matchList[index + 1])
+        setIndex(() => index + 1)
+    }
+    function handleDislikeButton() {
+        setCurrPotentialMatch(matchList[index + 1])
+        setIndex(() => index + 1)
+    }
+
     return(
         <>
             <div>
-                <p>Name</p>
-                <p>employer</p>
-                <p>start location</p>
-                <p>End location</p>
-                <p>Distance from user start</p>
-                <p>Distance from user end</p>
+                <p>Name: {`${currPotentialMatch.firstName} ${currPotentialMatch.lastName}`}</p>
+                <p>Employer: {currPotentialMatch.employer}</p>
+                <p>Start location: {currPotentialMatch.startLocation}</p>
+                <p>End location: {currPotentialMatch.endLocation}</p>
+                <p>Distance from your starting location: {currPotentialMatch.startDistanceDelta}</p>
+                <p>Distance from your ending location: {currPotentialMatch.endDistanceDelta}</p>
                 <div>
-                    <button>up</button>
-                    <button>down</button>
+                    <button onClick = {handleLikeButton}>up</button>
+                    <button onClick = {handleDislikeButton}>down</button>
                 </div>
             </div>
         </>
